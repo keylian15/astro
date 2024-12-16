ef582f4f608642ccb4b6cbf9117fa200d3caa28d
Keylian Turbé <keylian.turbe@univ-littoral.fr>
2024-12-16 11:14:29 +0100

    ADD Choix differents

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from astropy.io import fits 
 import matplotlib.pyplot as plt 
 
-data = fits.getdata('./Tarantula_Nebula-halpha.fit')
+# ====== Variables Globales ======
+Halpha = './Tarantula_Nebula-halpha.fit'
+Oiii = './Tarantula_Nebula-oiii.fit'
+Sii = './Tarantula_Nebula-sii.fit'
+# ====== Fin Variables Globales ======
 
-plt.imshow(data, cmap= 'gray')
-plt.colorbar()
-plt.show()
+def afficher(chemin : str ) : 
+    """"""
+    
+    data = fits.getdata(chemin)
+
+    plt.imshow(data, cmap= 'gray')
+    plt.colorbar()
+    plt.show()
+    
+# Choix user.
+choix = input("Choissisez l'image :\n1 -> Halpha.\n2 -> Oiii.\n3 -> Sii.\n")
+if choix == '1' : 
+    afficher(Halpha)
+if choix == '2' : 
+    afficher(Oiii)
+if choix == '3' : 
+    afficher(Sii)
